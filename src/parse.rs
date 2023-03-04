5efd33e4ee56a2e1a5f3520183489a0b9e846ad0
Connor Hanley <connordhanley@gmail.com>
2023-03-03 20:38:58 -0600

    parsing beginnings

--- src/parse.rs
+++ src/parse.rs
@@ -1,6 +1,21 @@
+use std::ops::Range;
+
 use crate::concrete::*;
-use crate::lex::*;
+use crate::lex::Token;
 
 use chumsky::prelude::*;
 
-type Spanned<T> = (T, 
+type Span = Range<usize>;
+type Spanned<T> = (T, Span);
+
+pub fn literal_parser() -> impl Parser<Token, Spanned<Literal>, extra::Err<Rich<char, Span>>> {
+    select! {
+    Token::Int(i) => Literal::Int(i),
+    Token::Float(f) => Literal::Float(f),
+    Token::String(s) => Literal::String(s),
+    Token::True => Literal::Bool(true),
+    Token::False => Literal::Bool(false),
+    }
+    .recover_with(skip_then_retry_until([]))
+    .map_with_span(|tt, span| (tt, span))
+}
